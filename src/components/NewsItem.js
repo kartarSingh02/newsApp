91b10cadb2c89aee144afaebd111f4ab076dfd0a
Kartar Singh <k4kartar4520@gmail.com>
2023-09-01 16:51:07 +0530

    added states inside classes

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
   render() {
-    let {title, description} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          <img src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/AA7C/production/_130944634_daniellemcgahey.jpg" className="card-img-top" alt="..."/>
+          <img src={imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+            <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
           </div>
         </div>  
       </div>
