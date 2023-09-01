5b721e9615dc5849596acb4d8dc645ff8939ea1a
Kartar Singh <k4kartar4520@gmail.com>
2023-09-01 15:02:27 +0530

    added few more components

--- src/components/News.js
+++ src/components/News.js
@@ -5,14 +5,19 @@ export class News extends Component {
 
   render() {
     return (
-      <div>
-        This is a news component.
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className="container my-3">
+        <h2>NewsDekho - Top Headlines</h2>
+          <div className='row'>
+            <div className='col-md-4'>
+              <NewsItem title="news1" description="abhi check kr rhe bs nhi denge news koi"/>
+            </div>
+            <div className='col-md-4'>
+              <NewsItem title="news1" description="abhi check kr rhe bs nhi denge news koi"/>
+            </div>
+            <div className='col-md-4'>
+              <NewsItem title="news1" description="abhi check kr rhe bs nhi denge news koi"/>
+            </div>
+          </div>
       </div>
     )
   }
