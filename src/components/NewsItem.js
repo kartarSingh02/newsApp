bf896f822dad86bfa4e4a8218f59bad049218980
Kartar Singh <k4kartar4520@gmail.com>
2023-09-01 18:01:09 +0530

    fetching api to populate newsItems

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,7 +7,7 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..."/>
+          <img src={!imageUrl?"https://www.livemint.com/lm-img/img/2023/08/31/600x338/parliament_1693474263743_1693474270468.jpg":imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
