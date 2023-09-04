e34475d2340de25f0c9db435817ac0705b9282f9
Kartar Singh <k4kartar4520@gmail.com>
2023-09-04 18:41:30 +0530

    fetched categories via props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
-
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+// import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import { BrowserRouter as Main, Route, Routes } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
-    return (
+    return ( 
+      <>
       <div>
+        <Main>
         <NavBar/>
-        <News pageSize={10} country="in" category="sports" />
+          <Routes> 
+            <Route path="/" element={ <News  key="general" pageSize={10} country="in" category="general" />} /> 
+            <Route path="/business" element={<News key="business" pageSize={10} country="in" category="business" />} />
+            <Route path="/technology" element={<News key="technology" pageSize={10} country="in" category="technology" />} />
+            <Route path="/sports" element={<News key="sports" pageSize={10} country="in" category="sports" />} />
+            <Route path="/science" element={<News key="science" pageSize={10} country="in" category="science" />} />
+            <Route path="/health" element={<News key="health" pageSize={10} country="in" category="health" />} />
+            <Route path="/entertainment" element={<News key="entertainment" pageSize={10} country="in" category="entertainment" />} />
+            <Route path="/general" element={<News key="general" pageSize={10} country="in" category="general" />} />
+          </Routes>
+        </Main> 
       </div>
+      </>
     )
   }
 }
