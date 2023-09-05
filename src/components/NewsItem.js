2c0ddfc94df1ae9750f576539dea64f9f19973f3
Kartar Singh <k4kartar4520@gmail.com>
2023-09-05 14:24:22 +0530

    added date,time and author with source

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,14 +3,17 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
     return (
       <div className='my-3'>
         <div className="card">
+        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:"88%", zIndex:"1"}}>
+            {source}</span>
           <img src={!imageUrl?"https://www.livemint.com/lm-img/img/2023/08/31/600x338/parliament_1693474263743_1693474270468.jpg":imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-body-secondary">By {author?author:"Unknown"} on {new Date(date).toGMTString()}</small></p>
             <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
           </div>
         </div>  
